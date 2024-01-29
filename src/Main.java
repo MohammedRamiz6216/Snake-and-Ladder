aa090cd34adbddc90c8ddf872842d54c84361829
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-01-29 10:38:00 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+     Scanner sc = new Scanner(System.in);
+     String A = sc.next();
+     int PositionofplayerA=0;
+     int count=0;
+     int x;
+        System.out.println("Enter the Player 1 ");
     }
 }
