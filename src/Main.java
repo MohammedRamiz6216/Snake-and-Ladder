db2a17a8ec5003f67cd025ddd480c24d0e914ae6
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-01-29 11:56:27 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -15,18 +15,78 @@ public class Main {
 
 
         switch(PositionofplayerA)
-        {
-            case 10:
+        {    //for ladders
+            case 8:
                 System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=22;
                 System.out.println(A+"Now player reached position 22");
                 break;
 
 
-            case 18:
-                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=22;
-                System.out.println(A+"Now player reached position 22");
+            case 15:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=20;
+                System.out.println(A+"Now player reached position 20");
                 break;
+
+            case 19:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=27;
+                System.out.println(A+"Now player reached position 27");
+                break;
+
+            case 29:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=36;
+                System.out.println(A+"Now player reached position 36");
+                break;
+
+            case 38:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA= 54;
+                System.out.println(A+"Now player reached position 54");
+                break;
+
+            case 65:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=96;
+                System.out.println(A+"Now player reached position 96");
+                break;
+
+            case 71:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=92;
+                System.out.println(A+"Now player reached position 92");
+                break;
+            // for snakes
+
+            case 14:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=4;
+                System.out.println(A+"Now player reached position 4");
+                break;
+
+            case 21:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=13;
+                System.out.println(A+"Now player reached position 13");
+                break;
+
+            case 38:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=17;
+                System.out.println(A+"Now player reached position 17");
+                break;
+
+            case 59:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=32;
+                System.out.println(A+"Now player reached position 32");
+                break;
+
+            case 76:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=9;
+                System.out.println(A+"Now player reached position 9");
+                break;
+
+            case 92:
+                System.out.println(A+"Player 1 reaches to the base of a ladder "); PositionofplayerA=83;
+                System.out.println(A+"Now player reached position 83");
+                break;
+            default:break;
         }
+        count++;
+
         }
+        System.out.println("Total no of dice thrown by player 1:"+count);
     }
 }
